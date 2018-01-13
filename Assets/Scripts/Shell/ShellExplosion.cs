f300e6b1e5f47caa5238efb4c22f0064e2039394
Tom Wallace <thomaspwallace@gmail.com>
2018-01-12 20:17:01 -0500

    Initial commit of TANKS repo.

---
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class ShellExplosion : MonoBehaviour
+{
+    public LayerMask m_TankMask;
+    public ParticleSystem m_ExplosionParticles;       
+    public AudioSource m_ExplosionAudio;              
+    public float m_MaxDamage = 100f;                  
+    public float m_ExplosionForce = 1000f;            
+    public float m_MaxLifeTime = 2f;                  
+    public float m_ExplosionRadius = 5f;              
+
+
+    private void Start()
+    {
+        // By passing the time, then it will be auto destroyed after the time period
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Find all the tanks in an area around the shell and damage them.
+        // By passing in the layerMask, we restrict those found to only the colliders in the mask
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+        foreach(Collider collider in colliders)
+        {
+            Rigidbody targetRigidBody = collider.GetComponent<Rigidbody>();
+            if (!targetRigidBody)
+                continue;
+
+            targetRigidBody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+            TankHealth targetHealth = targetRigidBody.GetComponent<TankHealth>();
+            // Should never happen, but in case the game is extended
+            if (!targetHealth)
+                continue;
+
+            float damage = CalculateDamage(targetRigidBody.position);
+            targetHealth.TakeDamage(damage);
+        }
+
+        // We have to remove the component, or else it will vanish with its parent
+        m_ExplosionParticles.transform.parent = null;
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+        Destroy(gameObject);
+    }
+
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        // Calculate the amount of damage a target should take based on it's position.
+        Vector3 explosionToTarget = targetPosition - transform.position;
+        float explosionDistance = explosionToTarget.magnitude;
+
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+
+        float damage = relativeDistance * m_MaxDamage;
+
+        // Ensure that if we catch the edge of a tank, it does not "heal" the tank
+        return Mathf.Max(0f, damage);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,5 @@
+This project was initially started with the Unity TANKS tutorial.
+
+https://unity3d.com/learn/tutorials/projects/tanks-tutorial
+
+I have continued to evolve it, adding new features to explore the Unity framework.  Enjoy!
